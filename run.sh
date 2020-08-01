ba0b64b17045b108fde4dab95266a3e57ef7f6b5
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-08-01 23:39:21 +0900

    fix

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+pip install -e .
+
+python test/system_test.py
--- test/system_test.py
+++ test/system_test.py
@@ -5,20 +5,13 @@ from gochan_crawler.spiders.board_list import BoardListSpider
 from gochan_crawler.spiders.thread import ThreadSpider
 
 
-def test_board_list():
+def test_system():
     crawler_process = scrapy.crawler.CrawlerProcess()
     crawler_process.crawl(BoardListSpider)
     crawler_process.start()
-    # def test_board():
-    crawler_process = scrapy.crawler.CrawlerProcess()
-    crawler_process.crawl(BoardSpider, url='https://asahi.5ch.net/newsplus/')
-    crawler_process.start()
-    # def test_thread():
-    crawler_process = scrapy.crawler.CrawlerProcess()
-    crawler_process.crawl(ThreadSpider, url='https://asahi.5ch.net/test/read.cgi/newsplus/1596266486/l50')
-    crawler_process.start()
-
-# runner = CrawlerRunner()
-# d = runner.crawl(ThreadSpider)
-# d.addBoth(lambda _: reactor.stop())
-# reactor.run()
+    # crawler_process = scrapy.crawler.CrawlerProcess()
+    # crawler_process.crawl(BoardSpider, url='https://asahi.5ch.net/newsplus/')
+    # crawler_process.start()
+    # crawler_process = scrapy.crawler.CrawlerProcess()
+    # crawler_process.crawl(ThreadSpider, url='https://asahi.5ch.net/test/read.cgi/newsplus/1596266486/l50')
+    # crawler_process.start()
