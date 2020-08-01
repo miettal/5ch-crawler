f06fe3d87683e3e257f0161a9cb1e4beb754ffe6
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-08-01 16:42:14 +0900

    fix

--- test/system_test.py
+++ test/system_test.py
@@ -3,11 +3,11 @@ from twisted.internet import reactor
 
 from scrapy.crawler import CrawlerRunner
 
-from gochan_crawler.spiders.thread import ThreadCrawler
+from gochan_crawler.spiders.thread import ThreadSpider
 
 
 def test_system():
     runner = CrawlerRunner()
-    d = runner.crawl(ThreadCrawler)
+    d = runner.crawl(ThreadSpider)
     d.addBoth(lambda _: reactor.stop())
     reactor.run()
