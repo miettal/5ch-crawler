caa4962ec8a78ac2e897d1f827d44411f59f5798
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-08-01 20:14:21 +0900

    fix

--- test/system_test.py
+++ test/system_test.py
@@ -5,7 +5,7 @@ from gochan_crawler.spiders.thread import ThreadSpider
 
 def test_system():
     crawler_process = scrapy.crawler.CrawlerProcess()
-    crawler_process.crawle(ThreadSpider, url='https://asahi.5ch.net/test/read.cgi/newsplus/1596266486/l50')
+    crawler_process.crawl(ThreadSpider, url='https://asahi.5ch.net/test/read.cgi/newsplus/1596266486/l50')
     crawler_process.start()
 
     # runner = CrawlerRunner()
