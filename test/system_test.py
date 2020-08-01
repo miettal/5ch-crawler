e8a671f834cfefd4dcecc4ac3abca1d3beefbacb
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-08-01 23:07:01 +0900

    fix

--- test/system_test.py
+++ test/system_test.py
@@ -8,17 +8,15 @@ from gochan_crawler.spiders.thread import ThreadSpider
 def test_board_list():
     crawler_process = scrapy.crawler.CrawlerProcess()
     crawler_process.crawl(BoardListSpider)
-    crawler_process.start()
-
-def test_board():
-    crawler_process = scrapy.crawler.CrawlerProcess()
+    crawler_process.start(stop_after_crawl=False)
+    # def test_board():
+    # crawler_process = scrapy.crawler.CrawlerProcess()
     crawler_process.crawl(BoardSpider, url='https://asahi.5ch.net/newsplus/')
-    crawler_process.start()
-
-def test_thread():
-    crawler_process = scrapy.crawler.CrawlerProcess()
+    crawler_process.start(stop_after_crawl=False)
+    # def test_thread():
+    # crawler_process = scrapy.crawler.CrawlerProcess()
     crawler_process.crawl(ThreadSpider, url='https://asahi.5ch.net/test/read.cgi/newsplus/1596266486/l50')
-    crawler_process.start()
+    crawler_process.start(stop_after_crawl=False)
 
 # runner = CrawlerRunner()
 # d = runner.crawl(ThreadSpider)
