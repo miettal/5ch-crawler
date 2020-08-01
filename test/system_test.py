5d887070d3c0f667e3d93e950ed0c7147efb2104
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-08-01 19:48:23 +0900

    fix

--- test/system_test.py
+++ test/system_test.py
@@ -1,13 +1,14 @@
-
-from twisted.internet import reactor
-
-from scrapy.crawler import CrawlerRunner
+import scrapy
 
 from gochan_crawler.spiders.thread import ThreadSpider
 
 
 def test_system():
-    runner = CrawlerRunner()
-    d = runner.crawl(ThreadSpider)
-    d.addBoth(lambda _: reactor.stop())
-    reactor.run()
+    crawler_process = scrapy.crawler.CrawlerProcess()
+    crawler_process.crawle(ThreadSpider, url='https://asahi.5ch.net/test/read.cgi/newsplus/1596266486/l50')
+    crawler_process.start()
+
+    # runner = CrawlerRunner()
+    # d = runner.crawl(ThreadSpider)
+    # d.addBoth(lambda _: reactor.stop())
+    # reactor.run()
