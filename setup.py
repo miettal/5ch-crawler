292e6345fcff8ab3f9ee7ad71216c6c69e7219f7
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-08-01 16:27:31 +0900

    fix

---
+++ setup.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+from setuptools import find_packages
+from setuptools import setup
+
+
+setup(
+    name='5ch-crawler',
+    packages=find_packages(),
+    install_requires=[
+        'Scrapy',
+    ],
+)
