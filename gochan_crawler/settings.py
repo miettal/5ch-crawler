4d2c06e990e7dddbae0ec1a69377af8668b0495b
Hiromasa Ihara <iharahiromasa@gmail.com>
2020-08-15 20:18:28 +0900

    fix

--- gochan_crawler/settings.py
+++ gochan_crawler/settings.py
@@ -6,3 +6,5 @@ NEWSPIDER_MODULE = 'gochan_crawler.spiders'
 
 USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.104 Safari/537.36'
 RETRY_TIMES = 5
+DOWNLOAD_DELAY = 0
+DOWNLOAD_TIMEOUT = 1
